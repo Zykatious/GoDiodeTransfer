02dd86e3f154ed87cc1c49acbe8e2db23f49547e
Zykatious <30608121+Zykatious@users.noreply.github.com>
2018-06-15 23:22:35 +0100

    Uploaded source files
    Client and server source files.

---
+++ src/Client/Client.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	//Set flags
+	filePtr := flag.String("f", "", "File you wish to send.")
+	ipPtr := flag.String("l", "127.0.0.1", "IP to transfer a file to.")
+	portPtr := flag.String("p", "1234", "Port to transfer a file to")
+
+	flag.Parse()
+
+	if *filePtr == "" {
+		fmt.Print("Please enter a file to send\n")
+		os.Exit(1)
+	}
+
+	//Initialise UDP Connection
+	conn, err := net.Dial("udp", *ipPtr+":"+*portPtr)
+	if err != nil {
+		fmt.Printf("Error %v", err)
+		return
+	}
+
+	//Load file to memory
+	file, _ := ioutil.ReadFile(*filePtr)
+
+	//Generate hash
+	hash := md5.New()
+	hash.Write(file)
+
+	//Calculate total number of packets.
+	totalPackets := int(len(file) / 1472)
+
+	//Send control packet indicating a file transfer is in progress
+	var startPacket []byte
+	startPacket = append(startPacket, "!XxSENDxX!"...)
+	startPacket = append(startPacket, fmt.Sprintf("%10d\n", totalPackets)...)
+	startPacket = append(startPacket, hex.EncodeToString(hash.Sum(nil))...)
+	startPacket = append(startPacket, filepath.Base(string([]byte(*filePtr)))...)
+	conn.Write(startPacket)
+	fmt.Printf("MD5 Sum: %s\n", hex.EncodeToString(hash.Sum(nil)))
+	fmt.Printf("Total packets: %d\n", totalPackets)
+
+	//Send main body of packets
+	for i := 0; i < totalPackets; i++ {
+		conn.Write(file[i*1472 : (i*1472)+1472])
+
+		//Sleep a bit per packet to allow for transfer time
+		time.Sleep(500 * time.Microsecond)
+
+		//Calculate total percentage sent so far and output to screen
+		total := int(float32(i) / float32(totalPackets) * 100)
+		fmt.Printf("\rFile %s: %d%% sent", *filePtr, total)
+	}
+
+	//Send extra packet if it's needed at the end of a file transfer (Because file does not divide exactly by 1472 bytes)
+	if len(file)%1472 != 0 {
+		var finalPacket []byte
+		finalPacket = append(finalPacket, "!XxLASTxX!"...)
+		finalPacket = append(finalPacket, fmt.Sprintf("%8d\n", len(file[totalPackets*1472:len(file)]))...)
+		finalPacket = append(finalPacket, file[totalPackets*1472:len(file)+1]...)
+		fmt.Printf("\rFile %s: 100%% sent", *filePtr)
+
+		fmt.Print("\nFile sent.")
+		conn.Write(finalPacket)
+	}
+
+	//Send control packet indicating file transfer is complete
+	conn.Write([]byte("!XxDONExX!"))
+	conn.Close()
+}
---
+++ src/Server/Server.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bytes"
+	"crypto/md5"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	//Set flags
+	dirPtr := flag.String("d", "./", "Location you wish files to be saved.")
+	portPtr := flag.Int("p", 1234, "Port the server listens on.")
+	flag.Parse()
+
+	filename := ""
+	var file []byte
+	var command byte
+	recPackets := 0
+	totalPackets := 0
+	fileHash := ""
+
+	//Set receiving buffer size to 1500 as final packet may be bigger than 1472 due to headers.
+	packetBuffer := make([]byte, 1500)
+
+	addr := net.UDPAddr{
+		Port: *portPtr,
+		IP:   net.ParseIP("127.0.0.1"),
+	}
+	ser, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		fmt.Printf("Error %v\n", err)
+		return
+	}
+	for {
+		_, _, err := ser.ReadFromUDP(packetBuffer)
+
+		//Test for control packet to say a new file is being received.
+		if testEq(packetBuffer[0:10], []byte("!XxSENDxX!")) {
+			//Get total number of packets expected to be received and file hash
+			totalPackets, _ = strconv.Atoi(strings.TrimSpace(string(packetBuffer[10:20])))
+			fileHash = string(packetBuffer[21:53])
+
+			//Trim packet down by stripping null bytes and get receving file's filename
+			trimPacket := bytes.Trim(packetBuffer[53:len(packetBuffer)], "\x00")
+			filename = filepath.Base(string(trimPacket))
+
+			//Set command byte and reset counters / receiving file
+			command = 1
+			file = nil
+			recPackets = 0
+
+			fmt.Printf("\n\nReceiving file %s\n", filename)
+			fmt.Printf("Total packets %d", totalPackets)
+		}
+
+		//Test for control packet to say file transfer is complete.
+		if testEq(packetBuffer[0:10], []byte("!XxDONExX!")) {
+			//Generate hash on received file
+			hash := md5.New()
+			hash.Write(file)
+
+			//Check file hash matches sending file
+			if hex.EncodeToString(hash.Sum(nil)) == fileHash {
+				fmt.Print("\nFile received. Hash check passed.\n")
+				//Save file
+				err := ioutil.WriteFile(*dirPtr+filename, file, 0644)
+				if err != nil {
+					fmt.Printf("Error  %v", err)
+					continue
+				}
+				fmt.Printf("File saved to %s%s", *dirPtr, filename)
+			} else {
+				fmt.Print("\nFile transfer failed, hashes do not match.\n")
+			}
+
+			//Set command byte and reset counters / receiving file
+			command = 1
+			file = nil
+			recPackets = 0
+		}
+
+		//Control packet to say packet is the final one
+		if testEq(packetBuffer[0:10], []byte("!XxLASTxX!")) {
+
+			//Convert final length string to an integer
+			finalLengthString := strings.TrimSpace(string(packetBuffer[10:18]))
+			finalLength, err := strconv.Atoi(finalLengthString)
+			if err != nil {
+				fmt.Printf("Error  %v", err)
+				continue
+			}
+			//Append the correct part of the packet to the end of the file by offsetting the command header.
+			file = append(file, packetBuffer[19:finalLength+19]...)
+
+			//Set command byte to stop packet being added to file again
+			command = 1
+		}
+
+		//If command byte is not set, add packet to receving file.
+		if command != 1 {
+			file = append(file, packetBuffer[0:1472]...)
+			recPackets++
+			total := int(float32(recPackets) / float32(totalPackets) * 100)
+			fmt.Printf("\rFile %s: %d%% received", filename, total)
+		}
+
+		//Reset Command byte to zero and reset receiving packet buffer
+		command = 0
+		packetBuffer = make([]byte, 1500)
+
+		if err != nil {
+			fmt.Printf("Error  %v", err)
+			continue
+		}
+		continue
+	}
+}
+
+//testEq - Check if a byte array matches a second byte array
+func testEq(a, b []byte) bool {
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
